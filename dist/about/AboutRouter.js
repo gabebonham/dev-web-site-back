3a71de402f917581c4c6ddbd764b5c94c3fd8b3b
gabebonham <grotegabriel@gmail.com>
2024-12-10 13:37:24 -0300

    removed dist

--- dist/about/AboutRouter.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const AboutController_1 = require("./controllers/AboutController");
-const router = (0, express_1.Router)();
-router.get('', AboutController_1.getAboutController);
-router.put('', AboutController_1.updateAboutController);
-exports.default = router;
