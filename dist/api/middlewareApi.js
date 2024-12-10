c432f2b6da4ed273d7f3b8b561acef59fc544aad
gabebonham <grotegabriel@gmail.com>
2024-12-10 13:25:32 -0300

    asdggg

--- dist/api/middlewareApi.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const middleware = (req, res) => {
-    console.log('asdf');
-};
-exports.default = middleware;
--- dist/api/projects/services/ProjectsService.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getAllProjects = getAllProjects;
-function getAllProjects(req, res) {
-    return [];
-}
