780838f51f5801b18233969371c2bc48c9eafa10
gabebonham <grotegabriel@gmail.com>
2024-12-13 15:37:37 -0300

    fads

--- api/admin/controllers/AuthController.ts
+++ api/admin/controllers/AuthController.ts
@@ -4,9 +4,9 @@ import { authenticate, login } from '../services/AuthService';
 export async function loginController(req: Request, res: Response) {
 	const user = req.body;
 	const session = await login(user);
-	console.log(session);
+	console.log(JSON.stringify(session));
 	if (session) {
-		res.send(session);
+		res.send(JSON.stringify(session));
 	} else {
 		res.sendStatus(403);
 	}
--- api/index.ts
+++ api/index.ts
@@ -48,7 +48,7 @@ app.use('/api/competences', competencesRouter);
 app.use('/api/messages', messageRouter);
 app.use('/login', authRouter);
 
-app.listen(() => {
+app.listen(3001, () => {
 	console.log('on');
 });
 
