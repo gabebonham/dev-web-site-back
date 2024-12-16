05f6a378c31b423b40cf8c6dde6af07890b4b66e
gabebonham <grotegabriel@gmail.com>
2024-12-16 16:52:19 -0300

    ji

--- api/middlewareApi.ts
+++ api/middlewareApi.ts
@@ -5,7 +5,7 @@ import { authenticateController } from './admin/controllers/AuthController';
 import cookieParser from 'cookie-parser';
 
 const middleware = async (req: Request, res: Response, next: NextFunction) => {
-	const session = await req.cookies;
+	const session = await req.cookies.session;
 	console.log('token recebido - ' + session);
 	const isAuthorized = await authenticateController(session);
 	if (isAuthorized) {
