b12ce316248f1b8d40a0bdd7a8bfb021374855a5
gabebonham <grotegabriel@gmail.com>
2024-12-16 15:31:33 -0300

    kjj

--- api/index.ts
+++ api/index.ts
@@ -50,7 +50,7 @@ app.use('/api/competences', competencesRouter);
 app.use('/api/messages', messageRouter);
 app.use('/login', authRouter);
 
-app.listen('::', () => {
+app.listen(3000, '::', () => {
 	console.log('on');
 });
 
