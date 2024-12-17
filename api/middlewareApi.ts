f2a689e90a895fcc4c45c3e078ee017119343db4
gabebonham <grotegabriel@gmail.com>
2024-12-16 22:54:18 -0300

    ogr

--- api/middlewareApi.ts
+++ api/middlewareApi.ts
@@ -6,28 +6,12 @@ import cookieParser from 'cookie-parser';
 import cookie from 'cookie'; // Import the cookie library
 
 const middleware = async (req, res: Response, next: NextFunction) => {
-	console.log('header ' + req.header);
 	console.log('token recebido - ' + req.headers.cookie);
 	const session = getCookie(await req.headers.cookie);
 
 	console.log('token - ' + session);
 	const isAuthorized = await authenticateController(session);
 	if (isAuthorized) {
-		res.set(
-			'Access-Control-Allow-Origin',
-			'https://dev-web-site-front-production.up.railway.app',
-		); // Frontend domain
-		res.set('Access-Control-Allow-Credentials', 'true'); // Allow cookies (credentials)
-		res.set(
-			'Access-Control-Allow-Methods',
-			'GET, POST, PUT, DELETE, OPTIONS',
-		); // Allow methods
-		res.set(
-			'Access-Control-Allow-Headers',
-			'Content-Type, Authorization',
-		); // Allow these headers in requests
-
-		res.set('Accept', 'application/json');
 		next();
 	} else {
 		res.sendStatus(403);
