b1729e207e8f26bd75983dabeec5ed154ab35c96
gabebonham <grotegabriel@gmail.com>
2024-12-18 07:37:16 -0300

    kajg

--- api/competences/controllers/CompetencesController.ts
+++ api/competences/controllers/CompetencesController.ts
@@ -25,18 +25,7 @@ export async function deleteCompetenceByIdController(
 ) {
 	const { id } = req.params;
 	await deleteCompetenceById(Number.parseInt(id));
-	res.setHeader(
-		'Access-Control-Allow-Origin',
-		'https://dev-web-site-front-production.up.railway.app',
-	); // Frontend domain
-	res.setHeader('Access-Control-Allow-Credentials', 'true'); // Allow cookies (credentials)
-	res.setHeader(
-		'Access-Control-Allow-Methods',
-		'GET, POST, PUT, DELETE, OPTIONS',
-	); // Allow methods
-	res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Allow these headers in requests
 
-	res.setHeader('Accept', 'application/json');
 	res.json({ msg: 'ok' });
 	res.send({ msg: 'ok' });
 }
