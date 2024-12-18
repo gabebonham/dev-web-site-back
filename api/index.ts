30ba4cabc9dd16d4258e6512bc78d8443873b8ef
gabebonham <grotegabriel@gmail.com>
2024-12-18 17:16:54 -0300

    kajg

--- api/index.ts
+++ api/index.ts
@@ -34,6 +34,21 @@ app.post('/api/*', middleware);
 app.put('/api/*', middleware);
 app.delete('/api/*', middleware);
 app.patch('/api/*', middleware);
+app.options('/api/*', (req, res, next) => {
+	res.setHeader(
+		'Access-Control-Allow-Origin',
+		'https://dev-web-site-front-production.up.railway.app',
+	); // Frontend domain
+	res.setHeader('Access-Control-Allow-Credentials', 'true'); // Allow cookies (credentials)
+	res.setHeader(
+		'Access-Control-Allow-Methods',
+		'GET, POST, PUT, DELETE, OPTIONS,PATCH',
+	); // Allow methods
+	res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Allow these headers in requests
+
+	res.setHeader('Accept', 'application/json');
+	return next();
+});
 app.get('/**/*', (req, res, next) => {
 	res.setHeader(
 		'Access-Control-Allow-Origin',
