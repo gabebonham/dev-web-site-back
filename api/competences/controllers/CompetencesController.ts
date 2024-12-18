1e6ec66eedc7d0123881348d592ae1677d97e64d
gabebonham <grotegabriel@gmail.com>
2024-12-18 12:15:29 -0300

    kajg

--- api/competences/controllers/CompetencesController.ts
+++ api/competences/controllers/CompetencesController.ts
@@ -8,6 +8,18 @@ import {
 
 export async function getAllCompetencesController(req: Request, res: Response) {
 	const competences = await getAllCompetences();
+	res.setHeader(
+		'Access-Control-Allow-Origin',
+		'https://dev-web-site-front-production.up.railway.app',
+	); // Frontend domain
+	res.setHeader('Access-Control-Allow-Credentials', 'true'); // Allow cookies (credentials)
+	res.setHeader(
+		'Access-Control-Allow-Methods',
+		'GET, POST, PUT, DELETE, OPTIONS,PATCH',
+	); // Allow methods
+	res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Allow these headers in requests
+
+	res.setHeader('Accept', 'application/json');
 	res.send(competences);
 }
 
@@ -25,7 +37,18 @@ export async function deleteCompetenceByIdController(
 ) {
 	const { id } = req.params;
 	await deleteCompetenceById(Number.parseInt(id));
+	res.setHeader(
+		'Access-Control-Allow-Origin',
+		'https://dev-web-site-front-production.up.railway.app',
+	); // Frontend domain
+	res.setHeader('Access-Control-Allow-Credentials', 'true'); // Allow cookies (credentials)
+	res.setHeader(
+		'Access-Control-Allow-Methods',
+		'GET, POST, PUT, DELETE, OPTIONS,PATCH',
+	); // Allow methods
+	res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Allow these headers in requests
 
+	res.setHeader('Accept', 'application/json');
 	res.json({ msg: 'ok' });
 	res.send({ msg: 'ok' });
 }
--- api/middlewareApi.ts
+++ api/middlewareApi.ts
@@ -13,18 +13,6 @@ const middleware = async (req: Request, res: Response, next: NextFunction) => {
 	console.log('token - ' + session);
 	const isAuthorized = await authenticateController(session);
 	if (isAuthorized) {
-		res.setHeader(
-			'Access-Control-Allow-Origin',
-			'https://dev-web-site-front-production.up.railway.app',
-		); // Frontend domain
-		res.setHeader('Access-Control-Allow-Credentials', 'true'); // Allow cookies (credentials)
-		res.setHeader(
-			'Access-Control-Allow-Methods',
-			'GET, POST, PUT, DELETE, OPTIONS,PATCH',
-		); // Allow methods
-		res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Allow these headers in requests
-
-		res.setHeader('Accept', 'application/json');
 		return next();
 	} else {
 		res.sendStatus(403);
