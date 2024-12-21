837f3c827aa0c071b2274b3517f68105ae3756ba
gabebonham <grotegabriel@gmail.com>
2024-12-21 03:20:09 -0300

    ga

--- api/lib/HeadersSetter.ts
+++ api/lib/HeadersSetter.ts
@@ -1,5 +1,14 @@
 export function setHeaders(res) {
-	res.setHeader('Access-Control-Allow-Headers', 'authorization'); // Allow these headers in requests
-
+	res.setHeader(
+		'Access-Control-Allow-Origin',
+		'https://dev-web-site-front-production.up.railway.app/',
+	); // Frontend domain
+	res.setHeader('Access-Control-Allow-Credentials', 'true'); // Allow cookies (credentials)
+	res.setHeader(
+		'Access-Control-Allow-Methods',
+		'GET, POST, PUT, DELETE, OPTIONS,PATCH',
+	); // Allow methods
+	res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Allow these headers in requests
+	res.setHeader('Access-Control-Allow-Headers', 'authorization');
 	res.setHeader('Accept', 'application/json');
 }
