ce03b9c91f1e83582ae5ef10fc1b65b5fd43f8f4
gabebonham <grotegabriel@gmail.com>
2024-12-21 10:52:04 -0300

    ga

--- api/lib/HeadersSetter.ts
+++ api/lib/HeadersSetter.ts
@@ -1,6 +1,6 @@
 export async function setHeaders(req, res) {
 	const a = (await req.headers).host as string;
-	res.setHeader('Access-Control-Allow-Origin', a); // Frontend domain
+	res.setHeader('Access-Control-Allow-Origin', 'https://' + a); // Frontend domain
 	res.setHeader('Access-Control-Allow-Credentials', 'true'); // Allow cookies (credentials)
 	res.setHeader(
 		'Access-Control-Allow-Methods',
