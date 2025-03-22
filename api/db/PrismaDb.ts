fdd261e15f345e8ba7a266825941318f9599f057
gabebonham <76006574+gabebonham@users.noreply.github.com>
2025-03-22 18:50:22 -0300

    Update PrismaDb.ts

--- api/db/PrismaDb.ts
+++ api/db/PrismaDb.ts
@@ -13,7 +13,7 @@ declare global {
 	var prisma: PrismaClient | undefined;
 }
 
-const connectionString = `${process.env.DATABASE_URL}`;
+const connectionString = `${process.env.STORAGE_DATABASE_URL}`;
 
 const pool = new Pool({ connectionString });
 const adapter = new PrismaNeon(pool);
