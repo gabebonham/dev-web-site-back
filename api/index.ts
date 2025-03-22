474a47111dc31a0bad5e1c3fed6aaef9598e5545
gabebonham <76006574+gabebonham@users.noreply.github.com>
2025-03-22 19:54:28 -0300

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -268,7 +268,9 @@ app.put('/api/about', async (req: Request, res: Response) => {
 app.get('/toggle', (req: Request, res: Response) => {
 	res.sendStatus(200);
 });
-
+app.get('/', async (req, res) => {
+	res.sendStatus(401);
+});
 app.listen(3000, () => {
 	console.log(`Server is running`);
 });
