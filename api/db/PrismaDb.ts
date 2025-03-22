b4797221c33afcf12ec93a9009e4f9c2284db29f
gabebonham <76006574+gabebonham@users.noreply.github.com>
2025-03-22 18:51:26 -0300

    Update PrismaDb.ts

--- api/db/PrismaDb.ts
+++ api/db/PrismaDb.ts
@@ -1,22 +1,25 @@
 import { PrismaClient } from '@prisma/client';
 import { PrismaNeon } from '@prisma/adapter-neon';
-import { Pool, neonConfig } from '@neondatabase/serverless';
+import { Pool } from '@neondatabase/serverless';
 
-import ws from 'ws';
-neonConfig.webSocketConstructor = ws;
+// WebSocket is not needed for Vercel Serverless Functions
+// import ws from 'ws';
+// neonConfig.webSocketConstructor = ws;
 
-// To work in edge environments (Cloudflare Workers, Vercel Edge, etc.), enable querying over fetch
-neonConfig.poolQueryViaFetch = true;
+// Disable querying over fetch (not needed for Vercel Serverless Functions)
+// neonConfig.poolQueryViaFetch = true;
 
 // Type definitions
 declare global {
-	var prisma: PrismaClient | undefined;
+  var prisma: PrismaClient | undefined;
 }
 
 const connectionString = `${process.env.STORAGE_DATABASE_URL}`;
 
 const pool = new Pool({ connectionString });
 const adapter = new PrismaNeon(pool);
+
+// Use global.prisma to avoid creating multiple instances in development
 const prisma = global.prisma || new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV === 'production') global.prisma = prisma;
